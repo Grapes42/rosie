b78a466c46fe4b8c2b623c25d562dc43f8fddfcc
Grapes42 <f.inggrapes@gmail.com>
2024-07-15 11:53:03 +1200

    commiting for player movement changes

--- player.gd
+++ player.gd
@@ -7,8 +7,9 @@ var pitch_input := 0.0
 @onready var twist_pivot := $TwistPivot
 @onready var pitch_pivot := $TwistPivot/PitchPivot
 
-@onready var mesh := $MeshInstance3D
-@onready var collision := $CollisionShape3D
+@onready var mesh := $MeshPitch/MeshRoll/MeshInstance3D
+@onready var mesh_pitch := $MeshPitch
+@onready var mesh_roll := $MeshPitch/MeshRoll
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -19,9 +20,24 @@ func _process(delta: float) -> void:
 	var input := Vector3.ZERO
 	input.x = Input.get_axis("move_left", "move_right")
 	input.z = Input.get_axis("move_forward", "move_back")
-
+	
 	apply_central_force(twist_pivot.basis * input * 3000 * delta)
-
+		
+	if Input.is_action_just_pressed("roll"):
+		for i in range(360/10):
+			mesh_roll.rotate_object_local(Vector3.RIGHT, deg_to_rad(10))
+			await get_tree().create_timer(0.001).timeout
+			
+	if Input.is_action_just_pressed("pitch"):
+		for i in range(360/10):
+			mesh_pitch.rotate_object_local(Vector3.BACK, deg_to_rad(10))
+			await get_tree().create_timer(0.001).timeout
+		
+	if Input.is_action_just_pressed("yaw"):
+		for i in range(360/10):
+			mesh_pitch.rotate_y(deg_to_rad(10))
+			await get_tree().create_timer(0.001).timeout
+		
 	if Input.is_action_just_pressed("jump"):
 		apply_central_force(Vector3.UP * 1200)
 		
@@ -32,8 +48,7 @@ func _process(delta: float) -> void:
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		
 	twist_pivot.rotate_y(twist_input)
-	mesh.rotate_y(twist_input)
-	collision.rotate_y(twist_input)
+	mesh_pitch.rotate_y(twist_input)
 	pitch_pivot.rotate_x(pitch_input)
 	pitch_pivot.rotation.x = clamp(pitch_pivot.rotation.x, deg_to_rad(-50), deg_to_rad(50))
 	twist_input = 0.0
